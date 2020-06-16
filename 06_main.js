8def79fe2acaa4af4d4e2280b5fc84a6481ed4f9
me-daniel <63238224+me-daniel@users.noreply.github.com>
2020-06-16 19:58:15 +0200

    debug

--- 06_main.js
+++ 06_main.js
@@ -31,7 +31,7 @@ $("document").ready(function() {
             // Possible deployment methods are:
             // "debug" and "directLink"
             // As well as "MTurk", "MTurkSandbox" and "Prolific"
-            deployMethod: "directLink",
+            deployMethod: "debug",
             contact_email: "dmenzel@uos.de",
             prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
         },
